139bb746bd25c8753ae741edc15b9a1c166f802e
Elliot Saba <staticfloat@gmail.com>
2024-06-05 00:06:28 +0000

    Readline v1.0.0

--- src/Readline_jll.jl
+++ src/Readline_jll.jl
@@ -1,9 +1,5 @@
-# Use baremodule to shave off a few KB from the serialized `.ji` file
-baremodule Readline_jll
-using Base
-using Base: UUID
-import JLLWrappers
+module Readline_jll
+using LazyJLLWrappers
+@generate_jll_from_toml()
+end # module Readline_jll
 
-JLLWrappers.@generate_main_file_header("Readline")
-JLLWrappers.@generate_main_file("Readline", UUID("05236dd9-4125-5232-aa7c-9ec0c9b2c25a"))
-end  # module Readline_jll
